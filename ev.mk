34e3d55ede29be7b10fb3ebc2e44bde73ca47ec8
x10forevers <android.x10f@gmail.com>
2014-12-08 02:49:42 -0400

    ev bringup 2

--- ev.mk
+++ ev.mk
@@ -1,7 +1,16 @@
+# Call this first so apn list is actually copied
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/gsm.mk)
+
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common evervolv stuff.
 $(call inherit-product, vendor/ev/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := ev_falcon
+# Pull all dictionaries
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/dictionaries/intl.mk)
+
+PRODUCT_NAME    := ev_falcon
+PRODUCT_BRAND   := Motorola
+PRODUCT_DEVICE  := falcon
+PRODUCT_MODEL   := Moto G
+PRODUCT_MANUFACTURER := Motorola
